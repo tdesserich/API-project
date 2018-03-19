35f8379cb4bed9f42454965e4b738edf7d200ce2
Tracy Desserich <tdesserich@gmail.com>
2018-03-19 09:27:04 -0400

    Changed back to Google maps

---
+++ googlemaps.js
@@ -0,0 +1,18 @@
+if("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+
+    var latlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+    var myOptions = {
+      zoom: 8,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.TERRAIN,
+      disableDefaultUI: true,
+    }
+    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+    });
+} else {
+    var para = document.createElement('p');
+    para.textContent = 'Argh, no geolocation!';
+    document.body.appendChild(para);
+}
